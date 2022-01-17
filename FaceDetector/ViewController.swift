2fc434a3d5e9f73620f52819d6afeb5209e2d16e
Helge Stahlmann <helge@hstahlmann.com>
2022-01-17 14:02:09 +0100

    Initial Commit

---
+++ FaceDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceDetector
+//
+//  Created by Helge Stahlmann on 17.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
